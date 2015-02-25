625cc58aa81cfab43c0f29fb23e69e7c34f87bd5
yusef <jj5201@gmail.com>
2015-02-24 18:36:45 -0800

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  has_secure_password
-  validates :password, length: { minimum: 6 }
+  has_secure_password 
+  validates :password, length: { minimum: 6 }, allow_blank: true 
 
   # Returns the hash digest of the given string.
   def User.digest(string)
