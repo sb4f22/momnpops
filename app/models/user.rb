a00487cc63820ebb58535ddaa2d9e352c3e24287
yusef <jj5201@gmail.com>
2015-03-03 20:30:56 -0800

    Created Micropost Resource & Views

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password 
   validates :password, length: { minimum: 6 }, allow_blank: true 
+  has_many :microposts, dependent: :destroy
   
 
   # Returns the hash digest of the given string.
