89a94fda1205a47349e393426649825d287436de
yusef <jj5201@gmail.com>
2015-03-03 18:21:13 -0800

    1st attempt at fixing production mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,11 +62,11 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-    config.action_mailer.raise_delivery_errors = true 
-    config.action_mailer.delivery_method = :smtp
-    host = 'sample-app-520.herokuapp.com'
-    config.action_mailer.default_url_options = { host: host}
-    ActionMailer::Base.smtp_settings = {
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = '<your heroku app>.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
@@ -75,7 +75,6 @@ Rails.application.configure do
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
