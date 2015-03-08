348106c2a51afa2c0e923620fad99c462f4f57ba
yusef <jj5201@gmail.com>
2015-03-07 23:41:49 -0800

    tried fixing secrets.yml & carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAI5JECNMM5HGR5G7Q'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['sample-app-520']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
